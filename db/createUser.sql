f4a64492ed1a314158d54e548b3584b244ffb395
Mitchel Foster <mitchfoster3@gmail.com>
2017-07-21 11:47:07 -0600

    Progress made; working with a db not defined error on server.js

---
+++ db/createUser.sql
@@ -0,0 +1,2 @@
+insert into users (firstname, lastname, username, email, auth0id)
+vaules ($1, $2, $3, $4, $5);
--- server/server.js
+++ server/server.js
@@ -12,7 +12,6 @@ const ctl = require('./controller');
 const app = module.exports = express();
 const port = config.port || 3000;
 const connectionString = config.connectionString;
-massive( connectionString ).then(dbInstance => {app.set('db', dbInstance)});
 
 app.use(bodyParser.json());
 app.use(cors());
@@ -31,11 +30,34 @@ passport.use( new Auth0Strategy({
         callbackURL: config.auth0.callbackURL
     },
     (accessToken, refreshToken, extraParams, profile, done) => {
+        console.log(profile._json.clientID)
+        massive( connectionString ).then(dbInstance => {app.set('db', dbInstance)
+            db.getUser([profile._json.clientID]).then((err,user)=>{
+                if(user){
+                    done(null, user[0])
+                } else {
+                    db.createUser([profile.name.givenName, profile.name.familyName, profile._json.nickname, profile._json.email, profile._json.clientID]).then((err, user)=> {
+                        done(null, user[0])
+                    })
+                }
+            })
+    
+    });
         done(null, profile)
     }
 ))
 
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
 app.get('/auth', passport.authenticate('auth0'));
+app.get('/auth/callback', passport.authenticate('auth0', 
+{ successRedirect: 'http://localhost:3000/admin', failureRedirect: 'http://localhost:3000/login' }));
 
 app.get('/api/getblogposts', ctl.getBlogPosts)
 app.get('/api/getblogpost/:postid', ctl.getBlogPost)
