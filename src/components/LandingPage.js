4277dd5275f7bc89f21a0d7a5840e04ae00477cf
Mitchel Foster <mitchfoster3@gmail.com>
2017-07-28 12:35:17 -0600

    Progress made on Landing Page styling. Break for lunch.

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -6,7 +6,7 @@ import {Button} from 'react-bootstrap'
 class LandingPage extends Component {
     render() {
         return (
-            <div>
+            <div id='landing_page'>
                 <NavBar/>
                 <div className='mountain_div'>
                     <h1>PERSONAL SLEEP APNEA CARE</h1>
@@ -15,7 +15,17 @@ class LandingPage extends Component {
                         <Button className='contact'>SCHEDULE YOUR CONSULTATION</Button>
                     </Link>
                 </div>
-                    <h2>Veniam tempor enim sint labore dolor dolore enim reprehenderit quis fugiat nostrud aliqua. Ipsum ea deserunt consectetur occaecat. Deserunt officia aute non qui magna et.</h2>
+                <div style={{textAlign: 'center'}} className='landing_div'>
+                    <h2 style={{fontWeight: '500'}}>Welcome to Personal Sleep Apnea Care</h2>
+                </div>
+                <div style={{width: '75vw', marginLeft: '12.5vw', marginBottom: '75px'}}>
+                    <h3>Sit ex enim magna ipsum excepteur. Non esse nostrud occaecat exercitation. Mollit velit consequat Lorem quis ad officia tempor cupidatat deserunt laborum est qui eiusmod deserunt. Commodo aliquip Lorem est duis pariatur ex exercitation deserunt enim. Voluptate in exercitation do non. Excepteur ut excepteur eiusmod excepteur id quis laborum cillum magna deserunt pariatur in. Tempor excepteur proident id pariatur non in qui mollit amet ex nisi laboris laboris ea.</h3>
+                    <h3>Sit ex enim magna ipsum excepteur. Non esse nostrud occaecat exercitation. Mollit velit consequat Lorem quis ad officia tempor cupidatat deserunt laborum est qui eiusmod deserunt. Commodo aliquip Lorem est duis pariatur ex exercitation deserunt enim. Voluptate in exercitation do non. Excepteur ut excepteur eiusmod excepteur id quis laborum cillum magna deserunt pariatur in. Tempor excepteur proident id pariatur non in qui mollit amet ex nisi laboris laboris ea.</h3>
+                    <h3>Sit ex enim magna ipsum excepteur. Non esse nostrud occaecat exercitation. Mollit velit consequat Lorem quis ad officia tempor cupidatat deserunt laborum est qui eiusmod deserunt. Commodo aliquip Lorem est duis pariatur ex exercitation deserunt enim. Voluptate in exercitation do non. Excepteur ut excepteur eiusmod excepteur id quis laborum cillum magna deserunt pariatur in. Tempor excepteur proident id pariatur non in qui mollit amet ex nisi laboris laboris ea.</h3>
+                </div>
+                <div style={{textAlign: 'center', marginBottom: '50px'}} className='landing_div'>
+                    <h2 style={{fontWeight: '500'}}>Learn More</h2>
+                </div>
                 <div className='main_navagation'>
                     <div className='navagation_div'>FAQ</div>
                     <div className='navagation_div'>Treatment Options</div>
